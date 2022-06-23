fe10d6182c6fe199118836b0ed3bd6a493c6ada5
Lautaro Darretche <lautarodarretche@hotmail.es>
2022-06-23 13:41:14 -0300

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import productoscreados from './reducers/createProductR';
 import PerfilDelUsuarioR from './reducers/PerfilDelUsuarioR';
+import carrito from './reducers/carritoR';
 
 const reducer = combineReducers({
-    productoscreados, PerfilDelUsuarioR
+    carrito,
+    productoscreados, 
+    PerfilDelUsuarioR
 })
 
 export const store = createStore(

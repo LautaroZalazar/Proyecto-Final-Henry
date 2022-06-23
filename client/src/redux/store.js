c391ac6311eed99a96afc7218bd0cd7c268bff79
Emiliano-Rotta <95652037+Emiliano-Rotta@users.noreply.github.com>
2022-06-23 14:15:46 -0300

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import carrito from './reducers/carritoR';
+import products from './reducers/productsR';
+import carritoR from './reducers/detailProductR';
+import productName from './reducers/productNameR';
 import productoscreados from './reducers/createProductR';
 import PerfilDelUsuarioR from './reducers/PerfilDelUsuarioR';
-import carrito from './reducers/carritoR';
 
 const reducer = combineReducers({
     carrito,
+    products,
+    carritoR,
+    productName,
     productoscreados, 
     PerfilDelUsuarioR
 })
